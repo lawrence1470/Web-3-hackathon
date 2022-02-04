8fe517f77ea9fe2414a7476f74df685ec1e82f82
lawrence1470 <lawrence.nicastro1@gmail.com>
2022-02-04 18:28:44 -0500

    add auth code

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const Navigation = () => {
-    return (
-        <nav className="hidden lg:flex gap-12">
-            <a href="#" className="text-indigo-500 text-lg font-semibold">Home</a>
-            <a href="#"
-               className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Features</a>
-            <a href="#"
-               className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Pricing</a>
-            <a href="#"
-               className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">About</a>
-        </nav>
-    );
+  return (
+    <nav className="hidden lg:flex gap-12">
+      <Link href="/wallet">
+        <a className="text-indigo-500 text-lg font-semibold">Wallet</a>
+      </Link>
+      {/*<a href="#" className="text-indigo-500 text-lg font-semibold">*/}
+      {/*  Home*/}
+      {/*</a>*/}
+      {/*<a*/}
+      {/*  href="#"*/}
+      {/*  className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"*/}
+      {/*>*/}
+      {/*  Features*/}
+      {/*</a>*/}
+      {/*<a*/}
+      {/*  href="#"*/}
+      {/*  className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"*/}
+      {/*>*/}
+      {/*  Pricing*/}
+      {/*</a>*/}
+      {/*<a*/}
+      {/*  href="#"*/}
+      {/*  className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100"*/}
+      {/*>*/}
+      {/*  About*/}
+      {/*</a>*/}
+    </nav>
+  );
 };
 
 export default Navigation;
