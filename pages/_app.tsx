2fc9c599711e5d7d0f5bb27f618fb758d54e3e9c
lawrence1470 <lawrence.nicastro1@gmail.com>
2022-02-04 16:39:30 -0500

    add moralis package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {MoralisProvider} from "react-moralis";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <MoralisProvider appId="8RPYNZqZtWsADYpKTVManBpPYm7x7xZC2aoAJpEU" serverUrl="https://mvzokwb7tyoe.usemoralis.com:2053/server" initializeOnMount>
+            <Component {...pageProps} />
+        </MoralisProvider>)
+
 }
 
 export default MyApp
